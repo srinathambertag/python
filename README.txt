3039c9cbb3d670fda9a4bd18cacbba5be303fc16
Srinath <srinathrajath0@gmail.com>
2022-09-03 15:00:27 +0530

    Code changes

--- README.txt
+++ README.txt
@@ -1,2 +1,15 @@
-# python
-# new line
+# import csv
+import git
+import pandas as pd
+from pcardext import cd
+from sqlalchemy import create_engine, types
+
+git_dir = "https://github.com/srinathrajath/python.git"
+g = git.cmd.Git(git_dir)
+g.pull('origin', 'main')
+print('Code pulled')
+
+engine = create_engine('mysql://root:password@localhost/movies')
+df = pd.read_csv("/home/srinath/Documents/data.csv", sep=',', quotechar='\'', encoding='utf8')
+df.to_sql('movies', con=engine, index=False, if_exists='append')
+print('Data Dump Completed')
--- mysql.py
+++ mysql.py
@@ -1,20 +1,23 @@
-# import csv
 import git
 import pandas as pd
 from sqlalchemy import create_engine, types
 
+
+# Pull Code
+repo = git.Repo('../python')
 git_dir = "https://github.com/srinathrajath/python.git"
 g = git.cmd.Git(git_dir)
 g.pull('origin', 'main')
 print('Code pulled')
 
+# Dump Data
 engine = create_engine('mysql://root:password@localhost/movies')
-df = pd.read_csv("/home/srinath/Documents/data.csv", sep=',', quotechar='\'', encoding='utf8')
-df.to_sql('movies', con=engine, index=False, if_exists='append')
+df = pd.read_csv("/home/srinath/PycharmProjects/Mysql Dump/python/data.csv", sep=',', quotechar='\'', encoding='utf8')
+df.to_sql('movies', con=engine, index=False, if_exists='replace')
 print('Data Dump Completed')
-# import  git
-#
-# repo.git.add('--all')
-# repo.git.commit('-m', 'changes', author='test_user@test.com')
-# origin = repo.remote(name='origin')
-# origin.push()
+
+# Push Code
+repo.index.add("data.csv")
+repo.index.commit('Initial commit.')
+print('code commited')
+print(repo.remotes.origin.push())
