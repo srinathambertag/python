fec5b1093eb4ca53f3d45adda8570b78793fc952
srinathrajath <101086143+srinathrajath@users.noreply.github.com>
2022-09-05 14:03:19 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -11,7 +11,7 @@ g.pull('origin', 'main')
 print('Code pulled')
 
 # Dump Data
-engine = create_engine('mysql://admin:Admin@123@100.24.6.167/films')
+engine = create_engine('mysql://admin:Admin@123@52.90.32.47/films')
 df = pd.read_csv("/home/srinath/PycharmProjects/Mysql Dump/python/data.csv", sep=',', quotechar='\'', encoding='utf8')
 df.to_sql('movies', con=engine, index=False, if_exists='replace')
 print('Data Dump Completed')
